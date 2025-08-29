16466699bdd1ffbfae910a9618731736c04a29e8
Vincent <vincent.granie@2018.icam.fr>
2025-08-29 15:46:10 +0200

    fix tasksToClose update query

--- src/scripts/seed_tokens.ts
+++ src/scripts/seed_tokens.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient()
 async function seedPriceFeeds() {
   const tokens = [
     {
-      address: "0x6d7EFb67236AaAeC2005ec704Bf5d755dd0703c4".toLowerCase(),
+      address: "0xbd605Ad2010E12c16B0cd0F2B8FE3c6d90BB51E7".toLowerCase(),
       name: "USG",
       symbol: "USG",
     },
