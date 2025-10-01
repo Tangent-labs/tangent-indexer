74b9f0e16d37f48622ae35dece2580438d4ff68a
walk-on-me <rabifromthenight@gmail.com>
2025-10-01 10:22:25 +0200

    Add log to debug devops

--- src/scripts/db-seed/_index.ts
+++ src/scripts/db-seed/_index.ts
@@ -4,13 +4,14 @@ import { seedPriceFeeds } from "./seed_tokens"
 import { seedLPTasks } from "./seed_lp_tasks"
 import { seedVoteTasks } from "./seed_vote_tasks"
 import { AddressesJson, readJsonFile } from "utils/readGDrive"
-
+import * as dotenv from "dotenv"
+dotenv.config()
 
 const prisma = new PrismaClient()
 async function main() {
+    console.log(process.env.GOOGLE_ADDRESSES_FILE_ID)
 
     const addresses = await readJsonFile<AddressesJson>(process.env.GOOGLE_ADDRESSES_FILE_ID!.toString())
-
     await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
         await deploySQLFunctions(tx)
         await seedPriceFeeds(tx, addresses)
