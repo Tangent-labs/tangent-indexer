b94d96ba5c750648ae7ec4b8b1af77c6ac94099b
Vincent <vincent.granie@2018.icam.fr>
2025-10-14 14:31:08 +0200

    fix build

--- src/scripts/db-seed/seed_tokens.ts
+++ src/scripts/db-seed/seed_tokens.ts
@@ -15,34 +15,14 @@ export async function seedTokensToTrack(tx: TransactionPrisma, addresses: Addres
       symbol: "sUSG",
     },
     {
-      address: CURVE_CONTEXT.USDT_crvUSD.stakeDaoGauge.toLowerCase(),
-      name: "USDT_crvUSD stakeDaoGauge",
-      symbol: "USDT_crvUSD",
-    },
-    {
       address: CURVE_CONTEXT.USDT_crvUSD.curveLp.toLowerCase(),
       name: "USDT_crvUSD curveLp",
       symbol: "USDT_crvUSD",
     },
     {
-      address: CURVE_CONTEXT.USDC_crvUSD.stakeDaoGauge.toLowerCase(),
-      name: "USDC_crvUSD stakeDaoGauge",
-      symbol: "USDC_crvUSD",
-    },
-    {
       address: CURVE_CONTEXT.USDC_crvUSD.curveLp.toLowerCase(),
       name: "USDC_crvUSD curveLp",
       symbol: "USDC_crvUSD",
-    },
-    {
-      address: CURVE_CONTEXT.USDe_USDC.curveLp.toLowerCase(),
-      name: "USDe_USDC curveLp",
-      symbol: "USDe_USDC",
-    },
-    {
-      address: CURVE_CONTEXT.USDe_USDC.curveGauge.toLowerCase(),
-      name: "USDe_USDC gauge",
-      symbol: "USDe_USDC",
     },
   ]
   await tx.tracked_erc20.createMany({
