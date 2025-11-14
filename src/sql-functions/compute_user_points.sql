9746b79314fff3c1ef0583af9e01bebd4ffed4a4
Vincent <vincent.granie@2018.icam.fr>
2025-11-14 10:52:10 +0100

    fix lp_referral_points setting

--- src/sql-functions/compute_user_points.sql
+++ src/sql-functions/compute_user_points.sql
@@ -3,25 +3,26 @@ CREATE OR REPLACE FUNCTION points.compute_user_points(
   end_at timestamp
 )
 RETURNS void
-LANGUAGE sql
+LANGUAGE plpgsql
 AS $$
+BEGIN
+  PERFORM points.insert_missing_user_points();
 
-    SELECT points.insert_missing_user_points();
+  UPDATE points.lp_user_points up
+  SET
+    points         =  up.points + pt.points,
+    booster_points =  up.booster_points + pt.booster_points
+  FROM points.get_user_points_per_task(start_at, end_at) pt
+  WHERE up.user_address = pt.user_address
+    AND up.task_id      = pt.task_id;
 
-    UPDATE points.lp_user_points up
-    SET
-      points         =  up.points + pt.points ,
-      booster_points =  up.booster_points + pt.booster_points
-    FROM points.get_user_points_per_task(start_at, end_at) pt
-    WHERE up.user_address = pt.user_address
-      AND up.task_id      = pt.task_id;
-
-
-
-       UPDATE global."user" g
-       SET lp_referral_points = g.lp_referral_points + pt.godfather_points
-       FROM points.get_user_points_per_task(start_at, end_at) pt
-       WHERE g.id = pt.godfather_id;
-
-
+  UPDATE global."user" g
+  SET lp_referral_points = g.lp_referral_points + agg.gdfp
+  FROM (
+    SELECT godfather_id, SUM(godfather_points) AS gdfp
+    FROM points.get_user_points_per_task(start_at, end_at)
+    GROUP BY godfather_id
+  ) AS agg
+  WHERE g.id = agg.godfather_id;
+END;
 $$;
