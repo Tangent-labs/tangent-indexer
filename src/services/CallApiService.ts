a81c42404aba762980d5853c727ce9f5cdb26f3b
walk-on-me <rabifromthenight@gmail.com>
2026-01-19 16:03:16 +0100

    Remove useless comments

--- src/services/CallApiService.ts
+++ src/services/CallApiService.ts
@@ -297,7 +297,6 @@ export class CallApiService {
       const response = await axios.get("https://api-staking-v2-worker.stakedao.org/api/rest/getallvaultswithassets", {
         httpsAgent: agent, // ensures proper TLS connection
         headers: {
-          // Copy exactly from the working curl
           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " + "AppleWebKit/537.36 (KHTML, like Gecko) " + "Chrome/120.0.0.0 Safari/537.36",
           Accept: "application/json, text/plain, */*",
           "Accept-Language": "fr-FR,fr;q=0.9,en;q=0.8",
@@ -305,7 +304,7 @@ export class CallApiService {
           Origin: "https://stakedao.org",
           Connection: "keep-alive",
         },
-        timeout: 15000, // optional but recommended
+        timeout: 15000,
       })
       const data: StakeDaoApiReturn = await response.data
       console.log(data)
