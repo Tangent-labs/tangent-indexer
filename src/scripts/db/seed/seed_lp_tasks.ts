5c00fb2aaf77f230a3ed58f7cf91b6c6301c7a71
Trottoir <maxime.ranciere@2018.icam.fr>
2026-05-04 22:40:55 +0200

    Add more addresses to exclude from point program

--- src/scripts/db/seed/seed_lp_tasks.ts
+++ src/scripts/db/seed/seed_lp_tasks.ts
@@ -1,5 +1,4 @@
 import { Prisma } from "@prisma/client"
-import { PENDLE_POOLS } from "@tangent/defi-resources"
 import { CONVEX_LOCKER } from "@tangent/defi-resources/build/ressources/contracts/convex.js"
 import { CURVE_CONTEXT } from "@tangent/defi-resources/build/ressources/mappings/curveContext.js"
 import { ZeroAddress } from "ethers"
@@ -28,16 +27,20 @@ export async function seedLPTasksAndTrackedERC20(
     data: lpTasks,
   })
 
-  // TODO This needs to be update when we know the list
   const addressesToExclude = [
     ZeroAddress.toLowerCase(),
+
     CONVEX_LOCKER.toLowerCase(),
-    // StakeDao staking on Convex
+    // StakeDAO Locker
+    "0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6".toLowerCase(),
 
     // USDC-USG Sidecar
-    "0x0e074EB3A9481B9E35fbFf9cDF663A1297b6a2D3",
+    "0x0e074EB3A9481B9E35fbFf9cDF663A1297b6a2D3".toLowerCase(),
     // frxUSD-USG Sidecar
-    "0xF49C02627335346C451Be6b8Ab35e63f91F4Fd6A",
+    "0xF49C02627335346C451Be6b8Ab35e63f91F4Fd6A".toLowerCase(),
+
+    // sUSG hold USG and must be excluded
+    "0xf17d6f98a5c6eaa99d149079984119e0a4ef6900".toLowerCase(),
 
     // USG Curve LP holds USG
     CURVE_CONTEXT.USG_USDC.curveLp.toLowerCase(),
@@ -52,9 +55,9 @@ export async function seedLPTasksAndTrackedERC20(
     CURVE_CONTEXT.USG_frxUSD.curveGauge.toLowerCase(),
 
     // Remove Pendle Market because it holds PT
-    PENDLE_POOLS["sUSDe 27/11/25"].MARKET.toLowerCase(),
-    PENDLE_POOLS["sUSDe 27/11/25"].PT.toLowerCase(),
-    PENDLE_POOLS["sUSDe 27/11/25"].SY.toLowerCase(),
+    // PENDLE_POOLS["sUSDe 27/11/25"].MARKET.toLowerCase(),
+    // PENDLE_POOLS["sUSDe 27/11/25"].PT.toLowerCase(),
+    // PENDLE_POOLS["sUSDe 27/11/25"].SY.toLowerCase(),
   ].map((uE) => ({ user: uE }))
 
   await prisma.lp_points_users_excluded.createMany({
