a397999dbb7808cc8ff047ab9740c90a5e268760
Trottoir <maxime.ranciere@2018.icam.fr>
2026-05-25 15:28:14 +0200

    Fix case of cvgPEpe

--- src/services/boost/config.ts
+++ src/services/boost/config.ts
@@ -3,7 +3,7 @@ import { NumMap } from "./types.js"
 
 export const SNAPSHOT_BOOST_TOKENS = [
   "0xe127cE638293FA123Be79C25782a5652581Db234",
-  "0x822ee3715e2c15372e45a4a62376bf786ff45511",
+  "0x822ee3715e2c15372e45a4a62376bF786fF45511",
   "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2",
   "0xEC6B8A3F3605B083F7044C0F31f2cac0caf1d469",
   "0x08d23468A467d2bb86FaE0e32F247A26C7E2e994",
@@ -12,7 +12,7 @@ export const SNAPSHOT_BOOST_TOKENS = [
 
 export const ONCHAIN_BOOST_INFOS: { [key: string]: { min: bigint; boost: number; key: string } } = {
   "0xe127cE638293FA123Be79C25782a5652581Db234": { min: 1n, boost: 0.5, key: "LLAMA_NFT" }, // Llama NFT
-  "0x822ee3715e2c15372e45a4a62376bf786ff45511": { min: 1n, boost: 0.75, key: "CVG_PEPE" }, // CVG PEPE
+  "0x822ee3715e2c15372e45a4a62376bF786fF45511": { min: 1n, boost: 0.75, key: "CVG_PEPE" }, // CVG PEPE
   "0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2": { min: parseEther("2500"), boost: 0.25, key: "veCRV" }, // veCRV
   "0x72a19342e8F1838460eBFCCEf09F6585e32db86E": { min: parseEther("500"), boost: 0.25, key: "vlCVX" }, // vlCVX
   "0xEC6B8A3F3605B083F7044C0F31f2cac0caf1d469": { min: parseEther("25"), boost: 0.25, key: "veFXN" }, // veFXN
